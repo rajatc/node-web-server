5c5edd167d4fb30d0b6b7c32a489afd4103cc6b1
Rajat Choudhury <choudhuryrajat@gmail.com>
2017-08-23 16:00:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    })
+    next();
+});
+
+// app.use((request, response, next) => {
+//     response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));//servering a directory
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    // response.send('<h1>Hello Express!</h1>');
+    // response.send({
+    //     name: 'rajat',
+    //     likes: ['Guitar', 'Tennis']
+    // });
+    response.render('home.hbs',{
+        title: 'Home Page',
+        welcomeMessage: 'Welcome to my homepage'
+    });
+});
+
+app.get('/about', (req, res) => {
+    // res.send('About Page');
+    res.render('about.hbs', {
+        title: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle this request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
